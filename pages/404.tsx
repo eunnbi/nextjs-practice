52cd2fff13b9a96fd0c0acd546f25f765f83d172
Eunbi Kang <jenabill@naver.com>
2022-06-13 21:35:03 +0900

    Refactor: 404 페이지에 Button 컴포넌트 임포트하여 사용하도록 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import CustomHead from "../components/CustomHead";
+import { Button } from "../components/Button";
+import { useBack } from "../hooks/useBack";
 import styled from "styled-components";
 
 const NotFoundMain = styled.main`
@@ -10,29 +12,14 @@ const NotFoundMain = styled.main`
   height: 80vh;
 `;
 
-const Button = styled.button`
-  background-color: #fff;
-  border-radius: 5px;
-  border: 1px solid #000;
-  padding: 0.5rem 1rem;
-  font-size: 15px;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    background-color: #000;
-    color: #fff;
-  }
-`;
-
 const NotFound = () => {
-  const router = useRouter();
-  const onClick = () => router.back();
+  const { goBack } = useBack();
   return (
     <>
       <CustomHead title="Not Found!" />
       <NotFoundMain>
         <h1>Not Found</h1>
-        <Button onClick={onClick}>⬅️ Go Back</Button>
+        <Button onClick={goBack}>⬅️ Go Back</Button>
       </NotFoundMain>
     </>
   );
