339ad40bc9261f7390ac107f423340070b3b273b
Eunbi Kang <jenabill@naver.com>
2022-06-14 00:12:23 +0900

    Feat: About Page 구현

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,51 @@
 import type { NextPage } from "next";
-import CustomHead from "../components/CustomHead";
+import { Button } from "../components/common/Button";
+import CustomHead from "../components/common/CustomHead";
+import { ImGithub } from "react-icons/im";
+import styled from "styled-components";
+
+const AboutMain = styled.main`
+  padding: 1.5rem;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  p {
+    text-align: center;
+  }
+`;
+
+const Anchor = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  svg {
+    font-size: 20px;
+  }
+`;
 
 const About: NextPage = () => {
   return (
-    <div>
+    <>
       <CustomHead title="About" />
-      <h1>About</h1>
-    </div>
+      <AboutMain>
+        <p>
+          <span>NextJS</span>를 공부하며 간단한 영화 정보 애플리케이션🎬 을
+          만들었습니다.
+        </p>
+        <Button>
+          <Anchor
+            href="https://github.com/eunnbi/nextjs-practice"
+            target="_blank"
+          >
+            <ImGithub />
+            README 보러가기
+          </Anchor>
+        </Button>
+      </AboutMain>
+    </>
   );
 };
 
