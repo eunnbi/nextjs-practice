65379d43c2325041e08c80ff3b57b13dcb1a342e
Eunbi Kang <jenabill@naver.com>
2022-06-15 15:38:27 +0900

    Design: Button 컴포넌트에 다크모드 style 적용

--- components/common/Button.styled.tsx
+++ components/common/Button.styled.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  color: #000;
-  background-color: #fff;
+  color: ${({ theme }) => theme.textColor};
+  background-color: ${({ theme }) =>
+    theme.name === "light" ? "#fff" : "#000"};
   border-radius: 5px;
   border: 1px solid #000;
   padding: 0.5rem 1rem;
