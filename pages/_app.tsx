900cd82fb4a22e49331c70a25cf59eae5566df78
Eunbi Kang <jenabill@naver.com>
2022-06-15 15:33:54 +0900

    Feat: ThemeProvider 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/common/Layout";
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../styles/theme";
+import { GlobalStyle } from "../styles/GlobalStyle";
+import Layout from "../components/common/Layout";
+import ThemeButton from "../components/common/ThemeButton";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+  const changeTheme = () => {
+    setIsLightTheme(!isLightTheme);
+  };
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+        <GlobalStyle />
+        <ThemeButton isLightTheme={isLightTheme} changeTheme={changeTheme} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
