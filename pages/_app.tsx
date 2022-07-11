eda3a4931a60fec1715216b4b6fa2b860d4a77cf
Eunbi Kang <jenabill@naver.com>
2022-07-11 16:30:02 +0900

    Refactor: remember theme using localStorage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../styles/theme";
@@ -9,12 +9,20 @@ import Layout from "../components/common/Layout";
 import ThemeButton from "../components/common/ThemeButton";
 
 const queryClient = new QueryClient();
+const THEME = "theme" as const;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isLightTheme, setIsLightTheme] = useState(true);
   const changeTheme = () => {
+    localStorage.setItem(THEME, JSON.stringify(!isLightTheme));
     setIsLightTheme(!isLightTheme);
   };
+  useEffect(() => {
+    const theme = localStorage.getItem(THEME);
+    if (theme !== null) {
+      setIsLightTheme(JSON.parse(theme));
+    }
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
