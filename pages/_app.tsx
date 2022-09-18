e4fe43177f8dc106e2dca631eeb7dd3c81784483
Eunbi Kang <jenabill@naver.com>
2022-09-19 00:31:40 +0900

    refactor: use useTheme hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../styles/theme";
 import { GlobalStyle } from "../styles/GlobalStyle";
 import Layout from "../components/Layout";
 import ThemeButton from "../components/ThemeButton";
+import { useTheme } from "../hooks/useTheme";
 
 const THEME = "theme" as const;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const [isLightTheme, setIsLightTheme] = useState(true);
-  const changeTheme = () => {
-    localStorage.setItem(THEME, JSON.stringify(!isLightTheme));
-    setIsLightTheme(!isLightTheme);
-  };
-  useEffect(() => {
-    const theme = localStorage.getItem(THEME);
-    if (theme !== null) {
-      setIsLightTheme(JSON.parse(theme));
-    }
-  }, []);
+  const { isLightTheme, changeTheme } = useTheme();
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
