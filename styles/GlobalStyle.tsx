5610bf5e23a1db1a83b547a9b6aaf8cbe63b9037
Eunbi Kang <jenabill@naver.com>
2022-09-20 17:02:55 +0900

    design: GlobalStyle 수정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -5,5 +5,20 @@ export const GlobalStyle = createGlobalStyle`
         background-color: ${(props) => props.theme.bgColor};
         color : ${(props) => props.theme.textColor}
     }
-
+    main {
+    padding: 20px;
+    }
+    h1,
+    h2,
+    h3 {
+    font-weight: bold;
+    }
+    h1 {
+        font-size: 32px;
+    }
+    p {
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+        word-break: keep-all;
+    }
 `;
