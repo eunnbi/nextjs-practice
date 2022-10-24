063c3dc2cc964c895f25780a84f5ef7ca787158f
Eunbi Kang <jenabill@naver.com>
2022-10-24 23:45:52 +0900

    next.config.js 설정 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const path = require("path");
 const API_KEY = process.env.API_KEY;
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
@@ -14,7 +17,13 @@ const nextConfig = {
     return [
       {
         source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+        has: [
+          {
+            type: "query",
+            key: "page",
+          },
+        ],
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=:page`,
       },
       {
         source: "/api/movies/:id",
