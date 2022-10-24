927d4fafca8e74eb141dd9dbd96b22fca8accdb8
Eunbi Kang <jenabill@naver.com>
2022-10-24 22:44:36 +0900

    refactor: MyApp component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,28 @@
-import "../styles/globals.css";
+import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { RecoilRoot } from "recoil";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "../styles/theme";
-import { GlobalStyle } from "../styles/GlobalStyle";
-import Layout from "../components/Layout";
-import ThemeButton from "../components/ThemeButton";
-import { useTheme } from "../hooks/useTheme";
+import Layout from "@components/Layout";
+import ThemeButton from "@components/ThemeButton";
+import CustomThemeProvider from "@styles/CustomThemeProvider";
+import { GlobalStyle } from "@styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const { isLightTheme, changeTheme } = useTheme();
   const { navBar } = pageProps;
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
-          <GlobalStyle />
-          <ThemeButton isLightTheme={isLightTheme} changeTheme={changeTheme} />
-          <Layout navBar={navBar === undefined ? true : navBar}>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
+        <RecoilRoot>
+          <CustomThemeProvider>
+            <GlobalStyle />
+            <ThemeButton />
+            <Layout navBar={navBar === undefined ? true : navBar}>
+              <Component {...pageProps} />
+            </Layout>
+          </CustomThemeProvider>
+        </RecoilRoot>
       </Hydrate>
     </QueryClientProvider>
   );
