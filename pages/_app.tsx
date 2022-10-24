1b1dd9bcb185a784c6f25b4d02f38bcf26846ce2
Eunbi Kang <jenabill@naver.com>
2022-10-24 23:04:31 +0900

    refactor: queryClient staleTime 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,16 @@ import CustomThemeProvider from "@styles/CustomThemeProvider";
 import { GlobalStyle } from "@styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      })
+  );
   const { navBar } = pageProps;
   return (
     <QueryClientProvider client={queryClient}>
