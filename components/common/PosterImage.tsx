9ac204775da0b0956da24fadf1875dc9d1facf2c
Eunbi <jenabill@naver.com>
2022-12-26 23:31:10 +0900

    refactor: PosterImage component

--- components/common/PosterImage.tsx
+++ components/common/PosterImage.tsx
@@ -1,21 +1,26 @@
-import Image from "next/image";
-
+import Image, { ImageProps } from "next/image";
 import styled from "styled-components";
 
 const Img = styled(Image)`
-  height: auto !important;
-  position: relative !important;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   border-radius: 15px;
 `;
 
-const PosterImage = ({ src }: { src: string }) => {
+interface Props {
+  src: string;
+  title: string;
+  sizes?: ImageProps["sizes"];
+}
+
+const PosterImage = ({ src, title, sizes }: Props) => {
   return (
     <Img
       src={`https://image.tmdb.org/t/p/w500/${src}`}
-      layout="fill"
+      alt={`${title} poster`}
+      fill
       placeholder="blur"
       blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOcOnt2PQAF5AJMrzp1XwAAAABJRU5ErkJggg=="
+      sizes={sizes}
     />
   );
 };
