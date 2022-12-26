b595a9ccdc49fa833b22780962bf0e56143e3341
Eunbi <jenabill@naver.com>
2022-12-26 22:23:04 +0900

    add useInnerHeight hook

---
+++ hooks/useInnerHeight.ts
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export const useInnerHeight = () => {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+};
