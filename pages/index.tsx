35949aec7cf68efa82bbf507a466535ecd6ce73b
Eunbi <jenabill@naver.com>
2022-12-26 23:31:27 +0900

    add scroll logic to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import CustomHead from "@components/common/CustomHead";
 import { moviesQuery } from "@api/movie";
 import { getAbsoluteUrl } from "@utils/getAbsoluteUrl";
 import MovieList from "@components/MovieList";
+import { getSessionStorage, SCROLL_POS_KEY } from "@utils/sessionStorage";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const value = getSessionStorage<number>(SCROLL_POS_KEY, 0);
+    window.scrollTo({
+      top: value,
+    });
+  }, []);
   return (
     <>
       <CustomHead title="Home" />
