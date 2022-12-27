f62c6430269ac2ac516ebdcd2a9397228c962a20
Eunbi <jenabill@naver.com>
2022-12-27 21:43:56 +0900

    delete: useInnerHeight hook

--- hooks/useInnerHeight.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export const useInnerHeight = () => {
-  useEffect(() => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
-};
