8dbf7c76ed30f0e02c2da3e7bec8f91a29b29f58
Eunbi <jenabill@naver.com>
2022-12-27 21:33:13 +0900

    fix: useInnerHeight hook

--- hooks/useInnerHeight.ts
+++ hooks/useInnerHeight.ts
@@ -4,5 +4,5 @@ export const useInnerHeight = () => {
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
+  }, []);
 };
