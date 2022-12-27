5217bb8fa82d4e365f581e487e53ebb433748af3
Eunbi <jenabill@naver.com>
2022-12-27 21:47:28 +0900

    design: 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,16 +2,18 @@ import CustomHead from "../components/common/CustomHead";
 import { Button } from "../components/common/Button.styled";
 import styled from "styled-components";
 import Router from "next/router";
-import { useInnerHeight } from "@hooks/useInnerHeight";
 
 const NotFound = () => {
-  useInnerHeight();
   const goHome = () => Router.push("/");
   return (
     <>
       <CustomHead title="Not Found!" />
       <Main>
-        <h1>Not Found</h1>
+        <h1>
+          404
+          <br />
+          Not Found
+        </h1>
         <Button onClick={goHome}>🏡 Go Home</Button>
       </Main>
     </>
@@ -21,12 +23,17 @@ const NotFound = () => {
 export default NotFound;
 
 const Main = styled.main`
+  position: fixed;
+  top: var(--header-height);
+  bottom: 0;
+  width: 100%;
+  max-width: var(--max-width);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: calc(100vh - var(--hh));
-  @media screen and (max-width: 900px) {
-    height: calc(var(--vh, 1vh) * 100 - var(--hh));
+  gap: 1.5rem;
+  h1 {
+    text-align: center;
   }
 `;
